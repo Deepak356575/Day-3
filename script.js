876b6d30951e5e76bfc6cf3950e94b389af50d4a
Deepak356575 <deepak356575@gmail.com>
2024-03-28 14:37:24 +0530

    Day 3 Task completed

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let obj2 = { age: 5, name: "person" };
 
 var a = JSON.stringify(obj1, Object.keys(obj1).sort());
 var b = JSON.stringify(obj2, Object.keys(obj2).sort());
-if (a === b) {
+if (a === b) 
+{
   console.log("The objects are equal");
 } else {
   console.log("The objects are not equal");
@@ -20,10 +21,12 @@ if (a === b) {
 var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
-request.onload = function () {
+request.onload = function () 
+{
   var data = request.response;
   var result = JSON.parse(data);
-  for (var i = 0; i < result.length; i = i + 1) {
+  for (var i = 0; i < result.length; i = i + 1) 
+  {
     console.log(result[i].flags.png);
   }
 };
@@ -35,10 +38,12 @@ request.onload = function () {
 var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
-request.onload = function () {
+request.onload = function () 
+{
   var data = request.response;
   var result = JSON.parse(data);
-  for (var i = 0; i < result.length; i = i + 1) {
+  for (var i = 0; i < result.length; i = i + 1)
+  {
     console.log(
       "Country name:" + " " + result[i].name.common,
       "region:" + " " + result[i].region,
